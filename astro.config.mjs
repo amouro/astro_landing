f779a527d07fddaf3b5714acd4c306986b11cf0c
chriswu <chris.wu@oist.jp>
2023-04-21 10:55:08 +0900

    Update contact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
+import preact from "@astrojs/preact";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amouro.github.io',
   integrations: [
+    preact(),
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
